36feea5d13d9bf064d02c0af31cc71ecf4b5a7ec
aluno <aluno@colegiomodulo.com.br>
2024-03-21 12:26:55 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt
+
+def ao_conectar(client, userdata, flags, rc):
+    print("Nós conectamos com o seguinte código de resultado {} ".format(str(rc)))
+
+def ao_receber(client, userdata, msg):
+    print("{} --- {}" .format(msg.topic, str(msg.payload)))
+
+cliente= mqtt.Client()
+
+cliente.on_connect = ao_conectar
+cliente.on_message = ao_receber
+cliente.connect("broker.hivemq.com", 1883, 60)
+cliente.subscribe("aula3c")
+cliente.loop_forever()
